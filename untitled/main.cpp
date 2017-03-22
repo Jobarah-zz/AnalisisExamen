f636f7c28a70ff511ec9856cf1695f800051547f
Jobarah <barahona.josue@unitec.edu>
2017-03-22 15:54:05 -0600

    countPaths not sure if really good

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -1,13 +1,49 @@
 #include "Test.h"
-#include <iostream>
 
 using namespace std;
 
 //Counts the number of possible paths from origin to destination in the given DAG (Directed Acyclic Graph)
 //Use Dynamic Programing to optimize the process
+
+int** initGraph(int size)
+{
+    int **answer = new int* [size];
+    for(int i = 0; i < size; i++)
+    {
+        answer[i] = new int[size];
+        for(int j = 0; j < size; j++)
+        {
+            answer[i][j] =- 1;
+        }
+    }
+    return answer;
+}
+
+int solve(int** DAG, int size, int origin, int destination, int ** helperGraph){
+
+    int returnVal = size <= 0 ? 0 : origin == destination ? 1 : size == 1 && DAG[origin][destination] ? 1 : -1, pathsCount = 0;
+
+    if (returnVal != -1)
+        return returnVal;
+
+    for (int i = 0; i < size; i++)
+        if (DAG[origin][i] != -1)
+        {
+            if(helperGraph[origin][i] == -1)
+            {
+                int temp = solve(DAG, size, i, destination, helperGraph);
+                helperGraph[origin][i] = temp;
+                pathsCount += temp;
+            } else{
+                pathsCount += helperGraph[origin][i];
+            }
+        }
+    return pathsCount;
+}
+
 int countPaths(int** DAG, int size, int origin, int destination)
 {
-    return -1;
+    return solve(DAG,size,origin,destination, initGraph(size));
 }
 
 //helper function to verify if a colour has been pushed in the vector returned by getVertexColors
@@ -26,7 +62,7 @@ bool answerContains(vector<int> answer, bool *available, int cr, int size)
     return true;
 }
 
-//helper flag every adjacent node's color as unavailable
+//helper function to flag every adjacent node's color as unavailable
 void processNeighboursColours(int **graph, bool *available, int iteration, int size)
 {
     for (int j = 0; j < size; j++)
@@ -50,6 +86,7 @@ int findFirstAvailableColour(bool * available, vector<int> answer, int size)
     }
 }
 
+//cleans available colours array
 void resetAvailableColours(int ** graph, bool* available, int iteration, int size)
 {
     for(int j =0; j < size; j++)
